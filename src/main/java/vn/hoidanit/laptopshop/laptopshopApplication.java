89873f945a77336b10060495e7f76dcd1079ae1e
Bui Minh Quan <bmq1603@gmail.com>
2026-02-01 00:44:39 +0700

    hello world

---
+++ src/main/java/vn/hoidanit/laptopshop/laptopshopApplication.java
@@ -0,0 +1,15 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class laptopshopApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(laptopshopApplication.class, args);
+
+    }
+
+}
