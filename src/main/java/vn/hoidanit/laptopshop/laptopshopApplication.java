aae6a02b5af735125cb5db705d68197138303cfd
Bui Minh Quan <bmq1603@gmail.com>
2026-02-02 16:43:04 +0700

    test : learn about javabeans

--- src/main/java/vn/hoidanit/laptopshop/laptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/laptopshopApplication.java
@@ -2,13 +2,19 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class laptopshopApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(laptopshopApplication.class, args);
+        // lấy ra biến
+        ApplicationContext hoidanit = SpringApplication.run(laptopshopApplication.class, args);
+        for (String s : hoidanit.getBeanDefinitionNames()) {
+            System.out.println(s);
+            ;
+        }
 
     }
 
