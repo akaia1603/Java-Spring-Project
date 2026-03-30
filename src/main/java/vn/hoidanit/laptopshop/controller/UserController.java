0990c65a8aeb22ea3cc31ca4fc15617083224b18
Bui Minh Quan <bmq1603@gmail.com>
2026-03-30 20:58:50 +0700

    render view

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,24 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.service.UserService;
 
 // class này biến thành controller
-@RestController
+@Controller
 public class UserController {
-    //Khai bao service để sử dụng
+    // Khai bao service để sử dụng
     private UserService userService;
+
     // Dùng inpendency injection
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-
-    
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
 
-
-
-
-    
 }
